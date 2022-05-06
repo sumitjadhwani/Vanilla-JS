f58e75c3b036dc0e8cd0c21dc9e4bc171e0a62cb
Sumit Jadhwani <42548520+sumitjadhwani@users.noreply.github.com>
2022-05-06 10:09:20 +0000

    Updated block-scope description

--- block-scope.js
+++ block-scope.js
@@ -1,6 +1,19 @@
-// var is function scoped
-// let & const are block scoped
-// Block has its own separate memory
+/* 
+1. var is function scoped
+2. let & const are block scoped
+3. Block has its own separate memory
+-----------------------------------------------------------------
+Shadowing: The concept of having same variable in other scope is known as shadowing.
+Here variable inside one scope shadows the variable of outer scope. 
+-----------------------------------------------------------------
+Illegal Shadowing: When one variable scope does interfere with other variable then it throws error and is known as Illegal Shadowing.
+eg. 
+let a = 5;
+{
+var a = 1; // This will throw error as var scope here is global.
+}
+*/
+
 console.log('------global scope-------');
 var g =1;
 {
