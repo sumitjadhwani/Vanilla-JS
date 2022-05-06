be16d0e4325622f6921a56b50af1c0e1c1a22e26
Sumit Jadhwani <42548520+sumitjadhwani@users.noreply.github.com>
2022-05-06 09:34:54 +0000

    Initial commit

---
+++ block.js
@@ -0,0 +1,16 @@
+// {....} is block in JS
+//Below is valid example of block
+{
+}
+
+//if else by default accepts only one executable statement
+if(true) console.log(450);
+
+
+//So, If we want to execute more statements we need to use BLOCK
+if(true) {
+	var x = 10;
+  var y = 1;
+  console.log(x);
+  console.log(y);
+}
