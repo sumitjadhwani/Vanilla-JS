922f9e4d75580868e441bd89b4b59a6f799e02a4
Sumit Jadhwani <42548520+sumitjadhwani@users.noreply.github.com>
2022-05-08 15:27:20 +0000

    Added functions

---
+++ functions.js
@@ -0,0 +1,9 @@
+/*
+All about different types of functions.
+1. Function Statement.
+2. Function Declaration.
+3. Function expression.
+4. Named function expression.
+5. Anonymous Function.
+6. First Class functions 
+*/
