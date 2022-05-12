dcb00244214a35491d441049c69a3f006c8620d8
Sumit Jadhwani <42548520+sumitjadhwani@users.noreply.github.com>
2022-05-12 17:34:36 +0000

    Event Loop 2

--- eventLoop.js
+++ eventLoop.js
@@ -6,3 +6,26 @@ In setTimer delay is the minimum time required for the runtime to process the re
 
 
 */
+(function() {
+
+  console.log('this is the start');
+
+  setTimeout(function cb() {
+    console.log('Callback 1: this is a msg from call back');
+  }); // has a default time value of 0
+
+  console.log('this is just a message');
+
+  setTimeout(function cb1() {
+    console.log('Callback 2: this is a msg from call back');
+  }, 0);
+
+  console.log('this is the end');
+
+})();
+
+// "this is the start"
+// "this is just a message"
+// "this is the end"
+// "Callback 1: this is a msg from call back"
+// "Callback 2: this is a msg from call back"
