3517f7829f15781063e4252b1d580a9218c200cb
Sumit Jadhwani <42548520+sumitjadhwani@users.noreply.github.com>
2022-05-12 17:20:48 +0000

    Event Loop 1

--- eventLoop.js
+++ eventLoop.js
@@ -1,9 +1,7 @@
 /*
 Event loop is responsible for executing the code, collecting and processing events, and executing queued sub-tasks.
 
-
-
-
+In setTimer delay is the minimum time required for the runtime to process the request (not a guaranteed time).
 
 
 
