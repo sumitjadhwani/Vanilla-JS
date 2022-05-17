34f61e26ad298dc7237d43831e4e45cfcfa80670
Sumit Jadhwani <42548520+sumitjadhwani@users.noreply.github.com>
2022-05-17 17:27:18 +0000

    Added object comparision and added strings in js

--- js-objects.js
+++ js-objects.js
@@ -9,3 +9,15 @@ const car = {
 
 console.log(car.brand);
 console.log(car.summary());
+
+
+
+
+x = new String('John');    
+y = new String('John');
+
+// Comparing two JS objects will always return FALSE
+// Beacause, In JavaScript, objects are a reference type. Two distinct objects are never equal, even if they have the same properties. Only comparing the same object reference with itself yields true.
+
+console.log(x == y);
+console.log(x === y);
---
+++ strings.js
@@ -0,0 +1,21 @@
+//Valid Strings
+let a = "Nikola Tesla";
+let b = 'Elon Musk';
+
+console.log(a);
+console.log(b);
+
+//Backslash
+/*
+Code	 Result	      Description
+\'	     '	          Single quote
+\"	     "	          Double quote
+\\	     \	           Backslash
+*/
+
+a = "JS is \"awesome\"";
+console.log(a);
+
+
+
+//JS Objects
